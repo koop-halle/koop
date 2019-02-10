edb2d35d6e277987d8092084fd479e22320a2d25
wolxXx <devops@wolxxx.de>
2019-02-10 17:15:13 +0100

    define before using

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
+defined('ROOT_DIR') || define('ROOT_DIR', __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR);
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('error_log', ROOT_DIR . DIRECTORY_SEPARATOR . 'error.log');
 
-defined('ROOT_DIR') || define('ROOT_DIR', __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR);
 require_once ROOT_DIR. DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'doctrineBootstrap.php';
 if (true === \Application\Util\DependencyContainer::getInstance()->getConfiguration()->isDevelopment()) {
