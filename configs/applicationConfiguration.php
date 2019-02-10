1dacbec762c191aa8a06445055d4b449e13ab5e1
wolxXx <devops@wolxxx.de>
2019-02-10 17:16:33 +0100

    added template application configuration

---
+++ configs/applicationConfiguration.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    \Application\Configuration::KEY_APPLICATION_NAME     => 'KKOP',
+    \Application\Configuration::KEY_APPLICATION_URL      => 'https://koop.de',
+    \Application\Configuration::KEY_IS_DEVELOPMENT       => false,
+];
